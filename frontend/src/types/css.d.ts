794a455afe57913941148820c4cab61056164785
s-shemmee <56132945+s-shemmee@users.noreply.github.com>
2025-09-28 18:17:45 +0100

    Enhance global styles and add CSS module declarations

---
+++ frontend/src/types/css.d.ts
@@ -0,0 +1,15 @@
+// CSS modules and global CSS imports declaration
+declare module '*.css' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: any;
+  export default content;
+}
